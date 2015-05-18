6c3d3ab44928f69a24efa68535f723bbd71ce4ee
dumatf <dumatf@users.noreply.github.com>
2015-05-18 01:23:58 +0000

    unit2 lesson3.3 - complete test suite

--- test/test-server.js
+++ test/test-server.js
@@ -13,6 +13,7 @@ describe('Shopping List', function() {
     chai.request(app)
         .get('/items')
         .end(function(err, res) {
+          //how to interact with the err parameter ??
           res.should.have.status(200);
           res.should.be.json;
           res.body.should.be.a('array');
@@ -28,8 +29,92 @@ describe('Shopping List', function() {
           done();
         });
   });
-  it('should add an item on post');
-  it('should edit an item on put');
-  it('should delete an item on delete');
-  it('should add a new item on put if one does not exist');
+  it('should add an item on POST', function(done) {
+    var itemsLength = storage.items.length
+    chai.request(app)
+        .post('/items')
+        .send({name: 'Eggs'})
+        .end(function(err, res) {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('id');
+          res.body.should.have.property('name');
+          res.body.id.should.be.a('number');
+          res.body.name.should.be.a('string');
+          res.body.name.should.equal('Eggs');
+          //storage length should increase by 1
+          storage.items.length.should.equal(itemsLength+1);
+          done();
+        });
+  });
+  it('should edit an item on PUT', function(done) {
+    var itemsLength = storage.items.length;
+    chai.request(app)
+        .put('/items/0')
+        .send({name: 'Bread'})
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('id');
+          res.body.should.have.property('name');
+          res.body.id.should.be.a('number');
+          res.body.name.should.be.a('string');
+          res.body.name.should.equal('Bread');
+          storage.items.length.should.equal(itemsLength);
+          done();
+        });
+  });
+  it('should delete an item on DELETE', function(done) {
+    var itemsLength = storage.items.length;
+    chai.request(app)
+        .delete('/items/1')
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('id');
+          res.body.should.have.property('name');
+          res.body.id.should.be.a('number');
+          res.body.name.should.be.a('string');
+          res.body.name.should.equal('Tomatoes');
+          storage.items.length.should.equal(itemsLength-1);
+          done()
+        });
+  });
+  it('should add new item on PUT if id dne', function(done) {
+    var itemsLength = storage.items.length;
+    chai.request(app)
+        .put('/items/1000')
+        .send({name: 'Onions'})
+        .end(function(err, res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property('id');
+          res.body.should.have.property('name');
+          res.body.id.should.be.a('number');
+          res.body.name.should.be.a('string');
+          res.body.name.should.equal('Onions');
+          storage.items.length.should.equal(itemsLength+1);
+          done();
+        });
+  });
+  it('should return an error on POST with no body', function(done) {
+    chai.request(app)
+        .post('/items')
+        .end(function(err, res) {
+          res.should.have.status(400);
+          done();
+        });
+  });
+  it('should return an error on PUT with no body', function(done) {
+    chai.request(app)
+        .put('/items/0')
+        .end(function(err, res) {
+          res.should.have.status(400);
+          done();
+        });
+  });
 });
